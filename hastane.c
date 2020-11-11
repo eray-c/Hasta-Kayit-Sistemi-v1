4e259fabd001ebf39d2a5e5ad1484153ed0b5569
eray-c <73905858+eray-c@users.noreply.github.com>
2020-11-11 23:26:30 +0300

    Update hastane.c

--- hastane.c
+++ hastane.c
@@ -2,76 +2,144 @@
 #include <string.h>
 #include "hastane.h"
 
-Hasta hastaListesi[UZUNLUK];
-int hastaSayisi = 0;
-int hastaEray = 0;
-int hastaOzge = 0;
-
-static void hastaOku(Hasta* hastaPtr);
-static void hastaYazdir(Hasta* hastaPtr);
+static void hastaOku(char[],char[],char[],char[],char[]);
 
 void hastaKayit() {
-    Hasta *hastaPtr;
-    if(hastaSayisi == UZUNLUK) {
-    printf("Tum randevular dolu\n");
-    return;
+    printf("\n\n\n");
+    printf("-------[HASTA KAYIT EKRANI]-------\n");
+    
+    char ad[AD],soyad[AD],tcNo[12],dogumTarihi[12],doktor[AD];
+    
+    hastaOku(ad,soyad,tcNo,dogumTarihi,doktor);
+    char dosyaAdi[AD];
+    char uzanti[5] = ".txt";
+    
+    strcpy(dosyaAdi,tcNo);
+    strcat(dosyaAdi,uzanti);
+    
+    FILE* filePtr = NULL;
+    filePtr = fopen(dosyaAdi,"w");
+    if(filePtr==NULL) {
+        printf("Kayit Olusturulamadi!\n");
     }
-    hastaPtr = &hastaListesi[hastaSayisi];
-    hastaOku(hastaPtr);
-    hastaSayisi++;
- }
-void kayitSil() {
-    Hasta *hastaPtr;
-    if(hastaSayisi == 0) {
-        printf("Kayitli hasta bulunmamaktadir!\n");
+    
+    fprintf(filePtr,"%s\n%s\n%s\n%s\n%s\n",ad,soyad,tcNo,dogumTarihi,doktor);
+    printf("Kayit Basariyla Olusturulmustur.\n");
+    fclose(filePtr);
+    
+    return;
+}
+
+void hastaSil() {
+    printf("\n\n\n");
+    printf("-------[KAYIT SILME EKRANI]-------\n");
+    
+    char tc[20];
+    char uzanti[5] = ".txt";
+    printf("Kaydi silinecek hastanin Tc sini giriniz :");
+    scanf("%s",tc);
+    strcat(tc,uzanti);
+
+    FILE* filePtr = NULL;
+    filePtr = fopen(tc,"r");
+    if(filePtr==NULL) {
+        printf("Kayit Bulunamadi!\n");
         return;
     }
-    hastaListele();
-    int silinecekID;
-    printf("\nKaydini silmek istediginiz hasta numarasini giriniz : ");
-    scanf("%d", &silinecekID);
-    while(silinecekID < 1 || silinecekID > hastaSayisi) {
-        printf("\nGirdiginiz ID gecersizdir!\n");
-        printf("Kaydini silmek istediginiz hasta numarasini giriniz :");
-        scanf("%d", &silinecekID);
-    }
-    hastaPtr = &hastaListesi[silinecekID - 1];
-    *hastaPtr = hastaListesi[hastaSayisi - 1];
-    hastaSayisi--;
-    printf("\nSectiginiz kayit silinmistir.\n");
+    
+    fclose(filePtr);
+    if(remove(tc)==0) {
+        printf("Kayit Basariyla Silindi.\n");
+        return;
+    } else
+        printf("Kayit Bulunamadi!\n");
+    return;
 }
-void hastaListele() {
-    Hasta *hastaPtr;
-    hastaPtr = hastaListesi;
-    int i = 0;
-    while(i<hastaSayisi) {
-        hastaYazdir(hastaPtr);
-        hastaPtr++;
-        i++;
+
+void hastaGuncelle() {
+    char yAd[AD],ySoyad[AD],yTcNo[12],yDogumTarihi[12],yDoktor[AD];
+    char tc[12];
+    char uzanti[5] = ".txt";
+    printf("Kaydi guncellenecek hastanin Tc sini giriniz :");
+    scanf("%s",tc);
+    strcat(tc,uzanti);
+    
+    FILE* filePtr = NULL;
+    filePtr = fopen(tc,"w");
+    if(filePtr==NULL) {
+        printf("Kayit Bulunamadi!\n");
+        return;
     }
-    printf("\n");
-}            
-static void hastaOku(Hasta* hastaPtr) {
-    printf("\n[HASTA EKLEME]\n");
+    hastaOku(yAd,ySoyad,yTcNo,yDogumTarihi,yDoktor);
+    fprintf(filePtr,"%s\n%s\n%s\n%s\n%s\n",yAd,ySoyad,yTcNo,yDogumTarihi,yDoktor);
+    printf("\nKayit basariyla guncellenmistir.\n");
+ 
+    fclose(filePtr);
+}
+
+void hastaSorgu() {
+    char ad[AD],soyad[AD],tcNo[12],dogumTarihi[12],doktor[AD];
+    char tc[12];
+    char uzanti[5] = ".txt";
     
-    printf("Hastanin adi-soyadi : ");
-    scanf("%s %s",(hastaPtr->ad),(hastaPtr->soyad));
+    printf("Kaydi goruntulenecek hastanin Tc sini giriniz : ");
+    scanf("%s",tc);
+    strcat(tc,uzanti);
     
-    printf("TC kimlik numarasi : ");
-    scanf("%s",(hastaPtr->tc));
+    FILE* filePtr = NULL;
+    filePtr = fopen(tc,"r");
+    if(filePtr==NULL) {
+        printf("Kayit Bulunamadi!\n");
+        return;
+    }
+    fscanf(filePtr,"%s\n%s\n%s\n%s\n%s\n",ad,soyad,tcNo,dogumTarihi,doktor);
+    fclose(filePtr);
     
-    printf("Hastanin dogum tarihi(gg.aa.yyyy) : ");
-    scanf("%s",&(hastaPtr->tarih));
+    printf("\n\n");
+    printf("Hastanin Adi : %s\n",ad);
+    printf("Hastanin Soyadi : %s\n",soyad);
+    printf("Hasta TC : %s\n",tcNo);
+    printf("Dogum Tarihi : %s\n",dogumTarihi);
+    printf("Muayene Oldugu Doktor : %s\n",doktor);
     
-    printf("Doktorun adi-soyadi : ");
-    scanf("%s %s",(hastaPtr->doktor.doktorAdi),hastaPtr->doktor.doktorSoyadi);
+}
+
+static void hastaOku(char ad[AD],char soyad[AD],char tcNo[12],char dogumTarihi[12],char doktorAd[AD]) {
+    char doktor1[AD] = "Eray Aynaci";
+    char doktor2[AD] = "Cabbar Yanbasan";
     
-}static void hastaYazdir(Hasta* hastaPtr) {
-    int hastaSiraNo = (hastaPtr - hastaListesi) + 1;
-    printf("\n");
-    printf("Hasta numarasi :%d\n",hastaSiraNo);
-    printf("Hasta adi :%s %s\n",hastaPtr->ad,hastaPtr->soyad);
-    printf("Hasta TC numarasi :%s\n",hastaPtr->tc);
-    printf("Hasta dogum tarihi :%s\n",hastaPtr->tarih);
-    printf("Hasta doktoru :%s %s\n",hastaPtr->doktor.doktorAdi,hastaPtr->doktor.doktorSoyadi);
+    printf("Hastanin adini giriniz : ");
+    scanf("%s",ad);
+    printf("Hastanin soyadini giriniz : ");
+    scanf("%s",soyad);
+    printf("Hastanin TC sini giriniz : ");
+    scanf("%s",tcNo);
+    
+    while(strlen(tcNo)!= 11) {
+        printf("Hasta TC si 11 haneli olmalidir!\n");
+        printf("Hastanin TC sini giriniz : ");
+        scanf("%s",tcNo);
+    }
+    
+    printf("Hastanin dogum tarihini giriniz (gg.aa.yyyy) :");
+    scanf("%s",dogumTarihi);
+    
+    char secim2;
+    printf("\n-----[DOKTOR LISTESI]-----\n");
+    printf("--->(1) %s\n",doktor1);
+    printf("--->(2) %s\n",doktor2);
+    printf("Kayit yaptirilacak doktorun kodunu giriniz :");
+    scanf("%s",&secim2);
+ 
+    switch(secim2) {
+        case '1':
+            strcpy(doktorAd,doktor1);
+            break;
+        case '2':
+            strcpy(doktorAd,doktor2);
+            break;
+        default:
+            printf("Lutfen Gecerli Bir Kod Giriniz!");
+            break;
+    }
 }
